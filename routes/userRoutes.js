fc356571a403bf49fba1443f1275f1013bce5ca5
yassen <yaseenseen9@gmail.com>
2024-04-15 08:18:07 +0530

    d error

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,33 +3,50 @@ const router = express.Router();
 const User = require("../models/User.model");
 
 router.post("/addseller", async (req, res) => {
-  const user = req.body;
-  const emailExists = await User.findOne({ email: user.email });
-  if (emailExists) return res.send("Email exists");
-  const newuser = await User.create(user);
-  res.json(newuser);
+  try {
+    const user = req.body;
+    const emailExists = await User.findOne({ email: user.email });
+    if (emailExists) return res.send("Email exists");
+    const newuser = await User.create(user);
+    res.json(newuser);
+  } catch (e) {
+    res.status(500).json({ err: "internal server error" });
+  }
 });
 
 router.post("/login", async (req, res) => {
-  const user = req.body;
-  const userfound = await User.findOne({ email: user.email });
-  if (!userfound) return res.status(401).json({ err: "user email not found" });
-  if (userfound.s_password != user.s_password) {
-    return res.status(401).json({ err: "invalid creds" });
+  try {
+    const user = req.body;
+    const userfound = await User.findOne({ email: user.email });
+    if (!userfound)
+      return res.status(401).json({ err: "user email not found" });
+    if (userfound.s_password != user.s_password) {
+      return res.status(401).json({ err: "invalid creds" });
+    }
+    return res.status(200).json(userfound);
+  } catch (e) {
+    res.status(500).json({ err: "internal server error" });
   }
-  return res.status(200).json(userfound);
 });
 
 router.get("/", async (req, res) => {
-  const seller = await User.find({});
-  res.json(seller);
+  try {
+    const seller = await User.find({});
+    res.json(seller);
+  } catch (e) {
+    res.status(500).json({ err: "internal server error" });
+  }
 });
 
 router.post("/seller", async (req, res) => {
-  const { seller } = req.body;
-  const sellerInfo = await User.find({ _id: seller });
-  if (!sellerInfo) return res.status(400).json({ err: "not found" });
-  return res.status(200).json(sellerInfo);
+  try {
+    const { seller } = req.body;
+    const sellerInfo = await User.find({ _id: seller });
+    if (!sellerInfo) return res.status(400).json({ err: "not found" });
+    return res.status(200).json(sellerInfo);
+  } catch (e) {
+    res.status(500).json({ err: "internal server error" });
+  }
 });
 
 module.exports = router;
